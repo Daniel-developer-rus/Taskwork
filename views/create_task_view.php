fdf0f1e5679e23ca0eb43bdd62d20431eeab18cc
Daniel-developer-rus <61740164+Daniel-developer-rus@users.noreply.github.com>
2020-06-14 17:51:09 +0400

    arregle el autocompletado en create task

--- views/create_task_view.php
+++ views/create_task_view.php
@@ -12,7 +12,7 @@
             <form action="create_task_controller.php" method="POST">
                 <input type="text" name="name" required placeholder="Insert Your Name" autofocus autocomplete="off">
                 <input type="email" name="email" required placeholder="Insert Your Email" autocomplete="off" >
-                <textarea name="description" placeholder="Insert Description" required></textarea>
+                <textarea name="description" placeholder="Insert Description" required autocomplete="off"></textarea>
                 <input type="submit" name="save_task" value="Create">
             </form>
 
